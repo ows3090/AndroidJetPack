ad24af262e16b9751f8d33b52fdc095d841cba34
ows3090 <ows3090@naver.com>
2021-02-23 23:02:52 +0900

    DataBinding with Java

--- java/RoomExample/app/src/main/java/ows/boostcourse/roomexample/MainActivity.java
+++ java/RoomExample/app/src/main/java/ows/boostcourse/roomexample/MainActivity.java
@@ -1,11 +1,14 @@
 package ows.boostcourse.roomexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.room.Room;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,36 +16,18 @@ import android.widget.TextView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import ows.boostcourse.roomexample.databinding.ActivityMainBinding;
 
-    private EditText mNameEditText;
-    private EditText mUniversityEditText;
-    private TextView mDbTextView;
-    MainViewModel viewModel;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.setLifecycleOwner(this);
 
-        mNameEditText = findViewById(R.id.name_edit);
-        mUniversityEditText = findViewById(R.id.university_edit);
-        mDbTextView = findViewById(R.id.db_text);
+        MainViewModel viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainViewModel.class);
+        binding.setViewmodel(viewModel);
 
-        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainViewModel.class);
-
-        // UI 갱신 (LiveData)
-        viewModel.getAll().observe(this, students -> {
-            mDbTextView.setText(students.toString());
-        });
-
-        Button insertButton = findViewById(R.id.insert_btn);
-        insertButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 버튼 클릭시 DB에 insert
-                viewModel.insertInfo(new Student(mNameEditText.getText().toString(),mUniversityEditText.getText().toString()));
-            }
-        });
     }
 }
--- java/RoomExample/app/src/main/java/ows/boostcourse/roomexample/MainViewModel.java
+++ java/RoomExample/app/src/main/java/ows/boostcourse/roomexample/MainViewModel.java
@@ -12,19 +12,24 @@ import java.util.List;
 
 public class MainViewModel extends AndroidViewModel {
     final AppDatabase db;
+    public LiveData<List<Student>> students;
+    public String name;
+    public String university;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
 
         db = Room.databaseBuilder(application,AppDatabase.class,"StudentInfo").build();
+        students = getAll();
     }
 
-    public LiveData<List<Student>> getAll(){
+    // Databinding 하는 부분에서 LiveData 호출 시 무한 루프 발생
+    private LiveData<List<Student>> getAll(){
         return db.studentDao().getAll();
     }
 
-    public void insertInfo(Student student){
-        new InsertAsyncTask(db.studentDao()).execute(student);
+    public void insertInfo(String name, String university){
+        new InsertAsyncTask(db.studentDao()).execute(new Student(name, university));
     }
 
     private static class InsertAsyncTask extends AsyncTask<Student,Void,Void>{
