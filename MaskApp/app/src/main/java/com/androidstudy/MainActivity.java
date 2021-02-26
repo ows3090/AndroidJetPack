28d4a5467db5e9a3e7c4ed3d23ea3a37677ae5ac
ows3090 <ows3090@naver.com>
2021-02-26 11:14:17 +0900

    Mask App java version

---
+++ MaskApp/app/src/main/java/com/androidstudy/MainActivity.java
@@ -0,0 +1,224 @@
+package com.androidstudy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidstudy.model.Store;
+import com.androidstudy.model.StoreInfo;
+import com.androidstudy.repository.MaskService;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+    private MainViewModel viewModel;
+    private FusedLocationProviderClient fusedLocationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainViewModel.class);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                performAction();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
+    }
+
+    @SuppressLint("MissingPermission")
+    public void performAction() {
+        fusedLocationClient.getLastLocation()
+                .addOnFailureListener(this, e -> {
+                    Log.e(TAG,"performAction : "+ e.getCause());
+                })
+                .addOnSuccessListener(this, location -> {
+                    // Got last known location. In some rare situations this can be null.
+                    if (location != null) {
+                        // Logic to handle location object
+                        Log.d(TAG,"getLatitude : " + location.getLatitude());
+                        Log.d(TAG,"getLongitude : "+location.getLongitude());
+
+                        location.setLatitude(37.188078);
+                        location.setLongitude(127.043002);
+                        viewModel.location = location;
+                        viewModel.fetchStoreInfo();
+                    }
+                });
+
+        RecyclerView recyclerView = findViewById(R.id.store_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
+
+        StoreAdapter adapter = new StoreAdapter();
+        recyclerView.setAdapter(adapter);
+
+        viewModel.itemLiveData.observe(this, stores -> {
+            adapter.updateItems(stores);
+            getSupportActionBar().setTitle("마스트 재고 있는 곳 : "+stores.size());
+        });
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        viewModel.loadingLiveData.observe(this, isLoading -> {
+            if(isLoading){
+                progressBar.setVisibility(View.VISIBLE);
+            }else{
+                progressBar.setVisibility(View.GONE);
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.menu.main_menu:
+                Log.d("Main","main_menu");
+                viewModel.fetchStoreInfo();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
+
+class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.StoreViewHolder>{
+
+    private List<Store> sItems = new ArrayList<>();
+
+    // 아이템 뷰의 정보를 가지고 있는 클래스
+    static class StoreViewHolder extends RecyclerView.ViewHolder{
+        TextView nameTextView;
+        TextView addressTextView;
+        TextView distanceTextView;
+        TextView remainTextView;
+        TextView countTextView;
+
+        public StoreViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nameTextView = itemView.findViewById(R.id.name_text_view);
+            addressTextView = itemView.findViewById(R.id.addr_text_view);
+            distanceTextView = itemView.findViewById(R.id.distance_text_view);
+            remainTextView = itemView.findViewById(R.id.remain_text_view);
+            countTextView = itemView.findViewById(R.id.count_text_view);
+        }
+    }
+
+    public void updateItems(List<Store> items){
+        sItems = items;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public StoreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_store,parent,false);
+        return new StoreViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StoreViewHolder holder, int position) {
+        Store store = sItems.get(position);
+
+        holder.nameTextView.setText(store.getName());
+        holder.addressTextView.setText(store.getAddr());
+        holder.distanceTextView.setText(String.format("%.2fkm",store.getDistance()));
+
+        String remainStat = "충분";
+        String count = "100개 이상";
+        int color = Color.GREEN;
+        switch (store.getRemainStat()){
+            case "plenty" :
+                remainStat = "충분";
+                count = "100개 이상";
+                color =  Color.GREEN;
+                break;
+            case "some":
+                remainStat = "여유";
+                count = "30개 이상";
+                color = Color.YELLOW;
+                break;
+            case "few":
+                remainStat = "매진 입박";
+                count = "2개 이상";
+                color = Color.RED;
+                break;
+            case "empty":
+                remainStat = "재고 없음";
+                count = "1개 이하";
+                color = Color.GRAY;
+                break;
+            default:
+        }
+        holder.remainTextView.setText(remainStat);
+        holder.countTextView.setText(count);
+        holder.remainTextView.setTextColor(color);
+        holder.countTextView.setTextColor(color);
+    }
+
+    @Override
+    public int getItemCount() {
+        return sItems.size();
+    }
+
+}
---
+++ MaskApp/app/src/main/java/com/androidstudy/MainViewModel.java
@@ -0,0 +1,79 @@
+package com.androidstudy;
+
+import android.location.Location;
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.LocationDistance;
+import com.androidstudy.model.Store;
+import com.androidstudy.model.StoreInfo;
+import com.androidstudy.repository.MaskService;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+public class MainViewModel extends ViewModel {
+    private static final String TAG = MainViewModel.class.getSimpleName();
+
+    public MutableLiveData<List<Store>> itemLiveData = new MutableLiveData<>();
+    public MutableLiveData<Boolean> loadingLiveData = new MutableLiveData<>();
+
+    public Location location;
+
+    private Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(MaskService.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build();
+
+    private MaskService service = retrofit.create(MaskService.class);
+
+
+    public void fetchStoreInfo() {
+        loadingLiveData.setValue(true);
+
+        // 로딩 시작
+       service.fetchStoreInfo(location.getLatitude(), location.getLongitude())
+                .enqueue(new Callback<StoreInfo>() {
+                    @Override
+                    public void onResponse(Call<StoreInfo> call, Response<StoreInfo> response) {
+                        Log.d(TAG, "onResponse");
+                        List<Store> items = response.body().getStores()
+                                .stream()
+                                .filter(item -> item.getRemainStat() != null)
+                                .filter(item -> !item.getRemainStat().equals("empty"))
+                                .collect(Collectors.toList());
+
+                        for(Store item : items){
+                            double distance = LocationDistance.distance(location.getLatitude(),location.getLongitude()
+                            ,item.getLat(),item.getLng(),"km");
+                            item.setDistance(distance);
+                        }
+                        Collections.sort(items);
+                        itemLiveData.postValue(items);
+
+                        // 로딩 끝
+                        loadingLiveData.postValue(false);
+                    }
+
+                    @Override
+                    public void onFailure(Call<StoreInfo> call, Throwable t) {
+                        Log.e(TAG, t.getMessage());
+                        itemLiveData.postValue(Collections.emptyList());
+
+                        // 로딩 끝
+                        loadingLiveData.postValue(false);
+                    }
+                });
+    }
+}
